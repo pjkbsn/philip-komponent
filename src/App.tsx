e125ccf6d2e7bd05414f64515199fa27c6daff13
Robin <Robin_Nygren@outlook.com>
2024-04-08 11:25:19 +0200

    added sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import Sidebar from "./components/Sidebar/Sidebar";
 import Toast from "./components/Toast/Toast";
 import "./styles.scss";
 
 const App = () => {
+  const items = ["Home", "About", "Services", "Contact"];
   return (
     <>
+      <Sidebar items={items} />
       <Toast message="Success" type="success" />
       <Toast message="ERROR" type="error" />
       <Toast message="InfoRutaheheheheheheeheh" type="info" />
